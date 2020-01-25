2c2ea0e89986213a1c9ddbab8bd63a4fe6618501
Josh Couper <joshcouper@gmail.com>
2020-01-25 20:22:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array(arr, elem)
-  arr.push(elem)
+[1, 2, 4, 5]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    arr.push(elem)
 end
 
 def add_element_to_start_of_array(array, element)
